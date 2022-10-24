5e48a239de5c7303a184d1988d099a966f07d10b
Doug Hill <dhill@captechventures.com>
2022-10-24 13:38:48 -0400

    Add button example

---
+++ src/components/Button/useButton.tsx
@@ -0,0 +1,6 @@
+// Should contain component state 
+// Should use useReducer within for complex domain logic
+// Can leverage several hooks
+// Should try to separate business state from presentational state (hidden, classNames, etc..)
+
+// DISCUSS: Put handlers in component or hook
